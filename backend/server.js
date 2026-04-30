265f293534ff19efbff8f96852bc87e8fd14312d
Lorin <lorin.hame@icloud.com>
2026-04-30 17:22:53 +0200

    final fix

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+// serve static files
+app.use(express.static(path.join(__dirname, "public", "frontend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "frontend", "index.html"));
 });
 
 const PORT = process.env.PORT || 10000;

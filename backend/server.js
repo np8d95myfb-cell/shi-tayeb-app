bee2aa5f1e1509950a4f6e4ece83ea0158867d49
Lorin <lorin.hame@icloud.com>
2026-04-30 17:30:14 +0200

    serve backend index

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,10 @@ const path = require("path");
 
 const app = express();
 
-// serve static files
-app.use(express.static(path.join(__dirname, "public", "frontend")));
+app.use(express.static(path.join(__dirname, "backend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "backend", "index.html"));
 });
 
 const PORT = process.env.PORT || 10000;

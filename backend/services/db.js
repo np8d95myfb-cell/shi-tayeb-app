3d4ff0339f699cd6a8b1aae01f3ef06a50587e99
Lorin <lorin.hame@icloud.com>
2026-04-30 16:28:41 +0200

    add backend files

---
+++ backend/services/db.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+function readJson(filePath, fallback = []) {
+  try {
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  } catch {
+    return fallback;
+  }
+}
+
+function writeJson(filePath, data) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+module.exports = { readJson, writeJson };
